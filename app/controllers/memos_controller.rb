86dfe5ab9b5371a4de7fb06205c1021229e7298c
Haruki Nagao <2111140017t@ed.fuk.kindai.ac.jp>
2023-12-06 06:52:53 +0000

    コントローラを変更

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -12,15 +12,6 @@ class MemosController < ApplicationController
     end
   end
 
-  def search
-    @memos = Memo.where('content LIKE ?', "%#{params[:password]}%")
-    if @memos.empty?
-      redirect_to top_index_path, alert: '合言葉もしくはURLが正しくありません'
-    else
-      render :main
-    end
-  end
-
   private
 
   def memo_params
