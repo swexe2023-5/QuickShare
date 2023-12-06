9b117aba9fc1ced3f6741728e871072f46b67a60
Haruki Nagao <2111140017t@ed.fuk.kindai.ac.jp>
2023-12-06 06:40:29 +0000

    綴りミス対処

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def search
-    @memos = Memo.where('content LIKE ?', "%#{params[:keyword]}%")
+    @memos = Memo.where('content LIKE ?', "%#{params[:password]}%")
     if @memos.empty?
       redirect_to top_index_path, alert: '合言葉もしくはURLが正しくありません'
     else
