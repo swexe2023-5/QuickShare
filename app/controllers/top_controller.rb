82ea0722a74cbaadf86719fd1a0d48ae02c4b3f0
Haruki Nagao <2111140017t@ed.fuk.kindai.ac.jp>
2023-12-13 06:17:21 +0000

    修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,17 +1,11 @@
 class TopController < ApplicationController
   def search
-    if valid_url?(params[:password])
-      # URLの場合、メモのID部分を抽出
-      memo_id = params[:password].split('/').last
-      redirect_to memo_path(memo_id)
+    hashed_password = BCrypt::Password.create(params[:password])
+    @memos = Memo.where(password: hashed_password)
+    if @memos.empty?
+      redirect_to top_index_path, alert: '合言葉もしくはURLが正しくありません'
     else
-      hashed_password = BCrypt::Password.create(params[:password])
-      @memos = Memo.where(password: hashed_password)
-      if @memos.empty?
-        redirect_to top_index_path, alert: '合言葉もしくはURLが正しくありません'
-      else
-        render :main
-      end
+      render :main
     end
   end
 
